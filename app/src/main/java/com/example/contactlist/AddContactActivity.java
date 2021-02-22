3285e1d551561bd9e3bcde6f2fe8177b1ecc4dce
Suat Ozcan <41187131+SuatOzcan@users.noreply.github.com>
2021-02-22 11:25:03 +0300

    Add menu to Contact List. Add items in menu.

--- app/src/main/java/com/example/contactlist/AddContactActivity.java
+++ app/src/main/java/com/example/contactlist/AddContactActivity.java
@@ -8,6 +8,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AddContactActivity extends AppCompatActivity {
@@ -22,5 +24,31 @@ public class AddContactActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("New Contact");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_contact, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }else if (id==R.id.action_delete_contact){
+            return true;
+        }else if (id==R.id.action_save_contact){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
